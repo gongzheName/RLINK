b7fd33cc6265e47902b3883590e2ab5ad1c19224
hezz <gongzhe_mail@163.com>
2018-01-26 19:55:09 +0800

    例行提交

--- FE/src/components/AdminOperation/link/LinkAdd.js
+++ FE/src/components/AdminOperation/link/LinkAdd.js
@@ -18,20 +18,55 @@ const userUpdUrl = "/admin/userUpdate";
 class RegistrationForm extends React.Component {
   constructor(props) {
     super(props);
-    let data = this.props.location.search;
+    let link_id = this.props.location.search; //获取base64编码
     let isUpdate = false;
-    data = parseInt(data.substr(1, ).split("=")[1]);
+    link_id = window.atob(link_id.substr(1, )); //解析
+    link_id = parseInt(link_id.split("=")[1]);
 
-    if (data > 0 && ((data | 0) === data)) {
-      console.log(data);
+    if (link_id > 0 && ((link_id | 0) === link_id)) {
       isUpdate = true;
+      let th = this;
+
+      axios.post("/admin/getLinkById",
+        qs.stringify({
+          request_id: "99",
+          link_id
+        })).then(function(data) {
+        let d = data.data.data[0]; //查询数据
+        let param1 = d.birth; //日期格式化
+        if (param1) {
+          param1 = new Date(param1);
+          let m = param1.getMonth();
+          if (m < 9) {
+            m = "0" + (m + 1);
+          } else {
+            m = m + 1;
+          }
+          param1 = param1.getFullYear() + "-" + m +
+            "-" + param1.getDate();
+          param1 = moment(param1, "YYYY-MM-DD");
+        } else {
+          param1 = null;
+        }
+        d.birth = param1;
+        th.props.form.setFieldsValue({
+          birth: d.birth,
+          email: d.email,
+          gender: d.gender,
+          name: d.name,
+          nickname: d.nickname,
+          introduce: d.introduce,
+          phone: d.phone
+        });
+      })
     }
 
     this.state = {
       confirmDirty: false,
       autoCompleteResult: [],
       isUpdate,
-      ctgrs:[]
+      ctgrs:[],
+      user_id
     };
   }
 
@@ -95,6 +130,7 @@ class RegistrationForm extends React.Component {
           return;
         }
 
+        values.user_id = "1";
         var msg_body = values;
         var request_data = {
           msg_body: JSON.stringify(msg_body),
@@ -103,7 +139,15 @@ class RegistrationForm extends React.Component {
         console.log('Received values of form: ', request_data);
         axios.post("/admin/linkAdd", qs.stringify(request_data))
           .then(function(data) {
-            console.log(data);
+            data = data.data;
+            if(data.resp_cd == "00"){
+              DialogModal.info(
+                "新增链接成功",
+                function(){
+                  window.location.href="#/link-mng";
+                }
+              )
+            }
           }).catch(function(err) {
             console.error(err);
           })
@@ -183,7 +227,7 @@ class RegistrationForm extends React.Component {
               message: '必填字段!',
             }],
           })(
-            <Input placeholder="请对您所添加的链接写一点介绍"/>
+            <Input readOnly={true} placeholder="请对您所添加的链接写一点介绍"/>
           )
           }
         </FormItem>
